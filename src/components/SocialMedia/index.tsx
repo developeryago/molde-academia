cac34121d2dc837895062b09a8b568155d2e4d14
Yago Machado <contato.yagomachado@outlook.com>
2024-08-26 21:16:38 -0300

    Feat Links

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -1,18 +1,18 @@
 import { Container } from './styles';
-import { FacebookIcon, InstagramIcon, SpotifyIcon } from '../CustomIcons/SocialIcons';
+import { FacebookIcon, InstagramIcon, MailIcon } from '../CustomIcons/SocialIcons';
 
 
 export function SocialMedia() {
   return (
     <Container>
-      <a href='https://www.instagram.com/lucaseduardo_crp/' target='_blank'>
+      <a href='https://www.instagram.com/tripleh_boston' target='_blank'>
         <InstagramIcon />
       </a>
-      <a href='https://www.linkedin.com/in/lucaseccarvalho/' target='_blank'>
+      <a href='https://api.whatsapp.com/send/?phone=17819249820&text&type=phone_number&app_absent=0' target='_blank'>
         <FacebookIcon />
       </a>
-      <a href='#'>
-        <SpotifyIcon />
+      <a href='https://linktr.ee/triplehconstruction' target='_blank'>
+        <MailIcon />
       </a>
     </Container>
   );
